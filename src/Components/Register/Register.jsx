794530a4ef94a9240b62f09aee799cf8abc2dc23
saurabh4323 <saurabhiitr01@gmail.com>
2024-07-08 20:14:29 +0530

    gg

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import "./Register.scss";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Button from "@mui/material/Button";
-import { useState } from "react";
 
 import { SignIn } from "@clerk/clerk-react";
 import Nav from "../Hero/Nav";
